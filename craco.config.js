06f33abdd10306582d1ab0f53205774bcb4828cd
VietNe <83568633+VietNe@users.noreply.github.com>
2021-08-01 13:41:10 +0700

    Setup project

---
+++ craco.config.js
@@ -0,0 +1,14 @@
+const path = require("path");
+
+module.exports = {
+  style: {
+    postcss: {
+      plugins: [require("tailwindcss"), require("autoprefixer")],
+    },
+  },
+  webpack: {
+    alias: {
+      "~styles": path.resolve(__dirname, "src/styles"),
+    },
+  },
+};
---
+++ src/pages/App.js
@@ -0,0 +1,11 @@
+import "~styles/colors.css";
+
+function App() {
+  return (
+    <div className='App'>
+      <h1 className='text-9xl test-color'>Test</h1>
+    </div>
+  );
+}
+
+export default App;
