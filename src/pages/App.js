239b519e78c80800e6191743c375a057940965c3
VietNe <83568633+VietNe@users.noreply.github.com>
2021-08-11 18:41:02 +0700

    Loader page

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,32 +1,40 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useState } from "react";
 import Div from "~components/Div";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Loader from "~components/Loader";
 
 import ProjectDetailsPage from "./ProjectDetailsPage";
 import AnimationLifecycle from "~containers/AnimationLifecycle";
 const Landing = React.lazy(() => import("./Landing"));
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
   return (
     <Div className='h-screen w-screen bg-pw-grey'>
       <Router>
         <Suspense fallback={null}>
           <Switch>
             <Route path='/'>
-              <Landing />
-              <Route
-                exact
-                path='/project/:projectSlug?'
-                children={(props) => (
-                  <AnimationLifecycle
-                    component={ProjectDetailsPage}
-                    whenToRender={(match) =>
-                      match && match.params && match.params.projectSlug
-                    }
-                    {...props}
+              {loading ? (
+                <Loader finishLoading={() => setLoading(false)} />
+              ) : (
+                <>
+                  <Landing />
+                  <Route
+                    exact
+                    path='/project/:projectSlug?'
+                    children={(props) => (
+                      <AnimationLifecycle
+                        component={ProjectDetailsPage}
+                        whenToRender={(match) =>
+                          match && match.params && match.params.projectSlug
+                        }
+                        {...props}
+                      />
+                    )}
                   />
-                )}
-              />
+                </>
+              )}
             </Route>
           </Switch>
         </Suspense>
